8b3611f8fe4122aa3907319c5e809e11f741bab2
OdairRosario <10days66@gmail.com>
2023-09-20 19:16:15 -0300

    remving $ from src of images

--- src/components/person/person.tsx
+++ src/components/person/person.tsx
@@ -2,7 +2,7 @@ const Person = () => {
     return (
         <div className="select-none non-interactive pointer-events-none hidden xl:flex xl:max-w-none">
         <img 
-        src={`$/Portifolio/avatar.png`}
+        src={`/Portifolio/avatar.png`}
         width={517} 
         height={458}
          alt=''
--- src/components/top-left-image/top-left-image.tsx
+++ src/components/top-left-image/top-left-image.tsx
@@ -3,7 +3,7 @@
 
 const TopLeftImage = () =>{
     return <div className="select-none non-interactive pointer-events-none absolute left-0 top-0 mix-blend-color-dodge z-10 w-[300px] xl:w-[300px] opacity-30 animate-pulse duration-75 ">
-        <img alt='' src={`$/Portifolio/top-left-img.png`} width={400} height={400}/>
+        <img alt='' src={`/Portifolio/top-left-img.png`} width={400} height={400}/>
             
 
     </div>
