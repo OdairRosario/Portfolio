914a03199330d1de7532c1e0649a37c18a1cce47
OdairRosario <10days66@gmail.com>
2023-09-20 19:39:15 -0300

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -25,10 +25,10 @@ const config: Config = {
         accent: '#F13024',
       },
       backgroundImage: {
-        explosion: 'url("/bg-explosion.png")',
-        circles: 'url("/bg-circles.png")',
-        circleStar: 'url("/circle-star.svg")',
-        site: 'url("/site-bg.svg")',
+        explosion: 'url("Portifolio/bg-explosion.png")',
+        circles: 'url("Portifolio/bg-circles.png")',
+        circleStar: 'url("Portifolio/circle-star.svg")',
+        site: 'url("Portifolio/site-bg.svg")',
       },
       animation: {
         'spin-slow': 'spin 6s linear infinite',
